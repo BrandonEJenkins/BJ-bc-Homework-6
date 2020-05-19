486b2cd18788d83458b79ea27beeed23531dbe2f
BrandonEJenkins <brandonejenkins@gmail.com>
2020-05-18 23:43:54 -0400

    updated js files to include two ajax calls

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ Weather app using ajax calls to fetch API data
 ## SOURCES
 https://stackoverflow.com/questions/20256516/highlight-list-item-in-bootstrap
 https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
+https://stackoverflow.com/questions/16086201/jquery-changing-style-of-html-element
+https://www.geeksforgeeks.org/jquery-create-a-div-element/
+https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_addclass
+https://teamtreehouse.com/community/why-do-we-add-event-to-the-anonymous-function
+https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
+https://stackoverflow.com/questions/43064850/using-ifelse-in-r-with-multiple-returns
+https://openweathermap.org/current
+http://www.javascripter.net/faq/mathsymbols.htm
+https://stackoverflow.com/questions/20024463/bootstrap-3-how-do-i-place-the-brand-in-the-center-of-the-navbar
+https://www.w3schools.com/css/css_dimension.asp
+https://www.w3schools.com/cssref/pr_pos_overflow.asp
+https://stackoverflow.com/questions/8398897/how-to-get-current-date-in-jquery
+https://mdbootstrap.com/snippets/jquery/mdbootstrap/939101
+https://www.eggradients.com/category/orange-gradient
+https://www.w3schools.com/cssref/css3_pr_box-shadow.asp
+https://stackoverflow.com/questions/24659471/nested-rows-with-bootstrap-grid-system
+https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/bootstrap-typography.php
+https://medium.com/@urchykli/nested-grids-using-bootstrap-8673b6bd7ec3
+https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+https://www.cognizantsoftvision.com/blog/handling-sequential-ajax-calls-using-jquery/
--- scripts.js
+++ scripts.js
@@ -18,94 +18,197 @@
     // Declare variable that gets city search text area for use in script
     // Declare variable that gets search button for use in script
 
-
 // Get html elements
 // var cityInputEl = $('#citySearchInput');
 // var searchButtonEl = $('#searchButton');
 
 // Click events
+$( document ).ready( function () {
 
+    var citySearchInput;
 
-
-
-
-// When #searcButton clicked run ajax call
-$('#searchButton').click(function(event) {
+    var stateSearchInput;
     
-    event.preventDefault();
+    var cityStore = window.localStorage.getItem("citySearchInput");
+    
+    var stateStore = window.localStorage.getItem("stateSearchInput");
+    
+    citySearchInput = cityStore;
+    stateSearchInput = stateStore;
 
-    alert('Search button was clicked...DELETE MY SCRIPT');
+    getWeather();
+    getFiveDay();
 
-    var citySearchInput = $('#citySearchInput').val();
-
-    var stateSearchInput = $('#stateSearchInput').val();
-
-    var apiKeyOWM = "bcc2fd2eebd337186fd819184e5d5181";    
-
-    if (citySearchInput === "" || stateSearchInput === "") {
-        alert('Must enter city AND state to get this bad boy rolling!  Otherwise, all you get is Houston, TX weather results!')
-        citySearchInput = "Houston"
-        stateSearchInput = "Texas"
-    }
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchInput + "," + stateSearchInput + "&units=imperial&APPID=" + apiKeyOWM;
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function (response) {
+    // When #searcButton clicked run ajax call
+    $('#searchButton').click(function(event) {
         
-        console.log(response);
-        console.log(response.main.temp);
-
-        var responseDiv = $("<div class='queryResult'>");
+        event.preventDefault();
     
-        var tempResponse = response.main.temp;
+        $('.fiveDayForecastDiv').empty();
 
-        var windResponse = response.wind.speed;
-
-        var humidityResponse = response.main.humidity;
-
-        var longLocation = response.coord.lon; // Store city longitude to be used in uv index call
-
-        var latLocation = response.coord.lat; // Store city latitude to be used in uv index call
-
-        // var pOne = $('<p>').text(citySearchInput + ", " + stateSearchInput);
-        var pOne = $('<p id="cityTextStyle">').text(citySearchInput + ", " + stateSearchInput);
+        citySearchInput = $('#citySearchInput').val();
     
-        var pTwo = $('<p>').text('Temperature: ' + tempResponse + "\xB0F");
-
-        var pThree = $('<p>').text('Wind Speed: ' + windResponse + "m/s");
-
-        var pFour = $('<p>').text('Humidity: ' + humidityResponse + "%");
-
-        // var pFive = $('<p>').text(wind.speed.value + " " + wind.speed.unit + " " + wind.speed.name);
-
-        var newDivBreak = $('<hr><br>');
+        stateSearchInput = $('#stateSearchInput').val(); 
     
-        responseDiv.append(pOne);
-
-        // $('pOne').css('font-size': '18px');
-
-        responseDiv.append(pTwo);
-
-        responseDiv.append(pThree);
-
-        responseDiv.append(pFour);
-
-        // responseDiv.append(pFive);
-
-        responseDiv.append(newDivBreak);
-        
-        $('#weatherSearchResult').append(responseDiv);
-
-        $('.list-group').append('<li class="list-group-item">' + citySearchInput + ", " + stateSearchInput + '</li>');
+        if (citySearchInput === "" || stateSearchInput === "") {
+            alert('Must enter city AND state to get this bad boy rolling!  Otherwise, all you get is Houston, TX weather results!')
+            citySearchInput = "Houston"
+            stateSearchInput = "Texas"
+        }
+    
+        window.localStorage.setItem("citySearchInput", citySearchInput);
+    
+        window.localStorage.setItem("stateSearchInput", stateSearchInput);
+    
+        getWeather();
+        getFiveDay();
     
     });
+    
+    
+    // window.onbeforeunload = function() {
+    //     this.localStorage.setItem("searchResult", $('#weatherSearchResult').val());
+    // }
+    
+    
+    function getWeather() {
+    
+        var apiKeyOWM = "bcc2fd2eebd337186fd819184e5d5181";   
+    
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchInput + "," + stateSearchInput + "&units=imperial&APPID=" + apiKeyOWM;
+    
+    
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            
+            console.log(response);
+            console.log(response.main.temp);
+    
+            var responseDiv = $("<div class='queryResult'>");
+        
+            var tempResponse = response.main.temp;
+    
+            var windResponse = response.wind.speed;
+    
+            var humidityResponse = response.main.humidity;
+    
+            var longLocation = response.coord.lon; // Store city longitude to be used in uv index call
+    
+            var latLocation = response.coord.lat; // Store city latitude to be used in uv index call
+    
+            // var date = new Date();
+            
+            // var month = date.getMonth() + 1;
+            // var day = date.getDate();
+            // var currentDate = date.getFullYear() + '/' +
+            //     (month < 10 ? '0' : '') + month + '/' +
+            //     (day < 10 ? '0' : '') + day;
+    
+            // var pOne = $('<p>').text(citySearchInput + ", " + stateSearchInput);
+            var pOne = $('<p id="cityTextStyle">').text(citySearchInput + ", " + stateSearchInput);
+        
+            var pTwo = $('<p>').text('Temperature: ' + tempResponse + "\xB0F");
+    
+            var pThree = $('<p>').text('Wind Speed: ' + windResponse + "m/s");
+    
+            var pFour = $('<p>').text('Humidity: ' + humidityResponse + "%");
+    
+            // var pFive = $('<p>').text(wind.speed.value + " " + wind.speed.unit + " " + wind.speed.name);
+    
+            // var pDate = $('<p id="currentDate">').text("(" + currentDate + ")");
+    
+            var newDivBreak = $('<hr><br>');
+        
+            responseDiv.append(pOne);
+    
+            // $('pOne').css('font-size': '18px');
+    
+            responseDiv.append(pTwo);
+    
+            responseDiv.append(pThree);
+    
+            responseDiv.append(pFour);
+    
+            // responseDiv.append(pFive);
+    
+            responseDiv.append(newDivBreak);
+            
+            // $('#weatherSearchResult').append(responseDiv + pDate);
+            $('#weatherSearchResult').append(responseDiv);
+    
+            $('weatherSearchResult').css("overflow-y", "scroll");
+    
+            $('.list-group').append('<li class="list-group-item">' + citySearchInput + ", " + stateSearchInput + '</li>');
+        
+        });
+    
+    }
+    
+    function getFiveDay() {
+    
+        var apiKeyOWM = "bcc2fd2eebd337186fd819184e5d5181";   
+    
+        // url query for 5 day forecast
+        var queryFiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchInput + "," + stateSearchInput + "&units=imperial&appid=" + apiKeyOWM;
+    
+            // ajax call for 5 day forecast
+            $.ajax({
+                url: queryFiveDayURL,
+                method: 'GET'
+            }).then(function(forecast) {
+                console.log(forecast);
+                // console log forecast items: date, an icon representation of weather conditions, the temperature, and the humidity
+                console.log(JSON.stringify(forecast.list[0].main.temp));
+                
+                for (let i = 0; i < forecast.list.length; i++) {
+                    
+                    var noon = "12:00:00";
+    
+                    var time = forecast.list[i].dt_txt;
+    
+                    if (time.includes(noon)) {
+    
+                        // parse object then append to div
+    
+                        // var forecastWeatherDescription = JSON.parse(forecast.list.weather.main);
+                        // var forecastWeatherDescription = JSON.parse(forecast);
 
-    console.group('Search Results');
-    console.log(citySearchInput + ", " + stateSearchInput);
-    console.groupEnd();
+                        // var p5DayWeather = $('<p>').text('Weather: ' + forecastWeatherDescription);
 
-});
+                        // $('.weatherCard').append(p5DayWeather);
+
+                        console.log(time);
+
+                        console.log(forecast.list[0].weather[0].main);
+
+                        // var forecastWeather = JSON.parse(forecast.list[0].weather[0].main);
+
+                        var forecastWeatherDiv = $('<div class="col p-2 mx-2 rounded-sm text-white  weatherCard">');
+
+                        var forecastWeatherEl = forecast.list[i].weather[0].main;
+
+                        var pForecastWeather = $('<p>').text('Weather: ' + forecastWeatherEl);
+
+                        forecastWeatherDiv.append(pForecastWeather);
+
+                        $('.fiveDayForecastDiv').append(forecastWeatherDiv);
+
+
+                        // var forecastWeather = forecast.list[0].weather[0].main;
+
+                        // var pWeather = $('<p class="forecastDivContents">').text('Conditions: ' + forecastWeather);
+
+                        // $('.weatherCard').append(pWeather);
+                    }
+    
+                }
+                console.log(JSON.stringify(forecast.list[0].main.temp));
+    
+                // var pForecast = forecast.split
+    
+            });
+    }
+})
 
